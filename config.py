c615f1ade3525c5643db783fb08caeca1a0b035d
nikkonrom <nikkonrom@gmail.com>
2017-09-03 22:11:03 +0300

    Redused amount of rate

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ domain_name = 'noshazambot.herokuapp.com'
 music_database_name = 'databases/music_database.db'
 users_database_name = 'databases/users_database.db'
 shelve_name = 'shelve.db'
-right_score = 100
+right_score = 10
